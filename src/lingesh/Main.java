b0df13a5a262537fa80d6e75849ee05d9cce32af
lingeshwaran05 <lingeskrish08@gmail.com>
2022-08-14 11:30:28 +0530

    first commit

---
+++ src/lingesh/Main.java
@@ -0,0 +1,17 @@
+package lingesh;
+
+import org.w3c.dom.ls.LSInput;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //Scanner sc=new Scanner(System.in);
+        byte a=50;
+        byte b=90;
+        int c=a*b;
+        int d='*';
+        System.out.println(d);
+
+    }
+}
